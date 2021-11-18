ccac01c9770ee72ac5ef70ffa93e8afed8c76ec6
GGINGHO <histolee96@naver.com>
2021-11-18 10:38:53 +0900

    11월 18일 오전

---
+++ CH02_variable/src/integerVariable/Doubles.java
@@ -0,0 +1,14 @@
+package integerVariable;
+
+public class Doubles {
+
+	public static void main(String[] args) {
+		// 실수 변수(소수점이 있는)를 선언
+		// int h = 1.8;
+		double h = 1.8;
+		
+		System.out.println("높이 h는" + h + "입니다.");
+
+	}
+
+}
